174ab78b72ecf51e2414e188df9997f46d7b4aeb
TechCiel <i@ciel.dev>
2023-08-22 19:59:55 +0800

    fancy: fix UTC modified time

--- public/_static/fancy.js
+++ public/_static/fancy.js
@@ -11,3 +11,13 @@ var app = {
         this.title = document.getElementsByTagName('h2')[0].textContent.slice('You are at '.length)
     }
 }
+
+/* HACK: get client timezone and fix UTC date */
+document.querySelectorAll(
+    'main table#list tbody tr:not(:first-child) td:nth-child(3)'
+).forEach((elm) => {
+    elm.innerText = new Date(
+        new Date(elm.innerText+' +0000')-new Date().getTimezoneOffset()*60*1000
+    ).toISOString().slice(0, 16).replace('T', ' ')
+})
+document.getElementById('list').classList.add('tzlocal')
