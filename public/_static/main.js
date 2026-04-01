e9a1f262bd2f8e88d397c2f9ae99c000974a0f2b
mirrors <mirrors@mirrors.jlu.edu.cn>
2026-04-01 14:26:21 +0800

    main: show "long time ago" after 90 days

--- public/_static/main.js
+++ public/_static/main.js
@@ -30,8 +30,7 @@ var app = {
             if (abs < (60 - future) + future) ret = this._.timeSeconds
             else if (abs < (60 * 60 - future) + future) ret = this._.timeMinute(round(abs / 60))
             else if (abs < (60 * 60 * 24 - future) + future) ret = this._.timeHour(round(abs / 60 / 60))
-            else if (abs < (60 * 60 * 24 * 365 - future) + future) ret = this._.timeDay(round(abs / 60 / 60 / 24))
-            else if (abs < (60 * 60 * 24 * 365 * 10 - future) + future) ret = this._.timeYear(round(abs / 60 / 60 / 24 / 365))
+            else if (abs < (60 * 60 * 24 * 90 - future) + future) ret = this._.timeDay(round(abs / 60 / 60 / 24))
             else ret = this._.timeMore
             if (parseInt(ret) > 1) ret = this._.timePlural(ret)
             return future ? this._.timeFuture(ret) : this._.timePast(ret)
